b6e86df594b14954b99a7562ab7481c371e620ea
Saif <saifad303@gmail.com>
2023-02-26 13:29:24 +0600

    project init with tailwind -i and signUp & signIn

---
+++ src/pages/sign-in/index.js
@@ -0,0 +1,7 @@
+export default function index() {
+  return (
+    <div>
+      <p>Sign in page</p>
+    </div>
+  );
+}
---
+++ src/pages/sign-up/index.js
@@ -0,0 +1,7 @@
+export default function index() {
+  return (
+    <div>
+      <p>sign up page</p>
+    </div>
+  );
+}
