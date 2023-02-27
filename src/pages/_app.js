fcb375b869cd8d9a1428e7a296ac580a8afa5b5b
Saif <saifad303@gmail.com>
2023-02-27 10:21:50 +0600

    set: dmSans to next fontFamily & tailwind font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { DM_Sans } from "next/font/google";
+
+const dmSans = DM_Sans({
+  weight: ["400", "500", "700"],
+  style: ["italic", "normal"],
+  subsets: ["latin"],
+  variable: "--font-dmSans",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return;
+  <main className={`${dmSans.variable} font-sans`}>
+    <Component {...pageProps} />
+  </main>;
 }
