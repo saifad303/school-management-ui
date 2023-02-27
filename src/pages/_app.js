d00b8da49aa147dfd80a4377d58df3168c873f7a
Saif <saifad303@gmail.com>
2023-02-27 11:58:10 +0600

    built: signUp mobile responsive

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,16 @@ import "@/styles/globals.css";
 import { DM_Sans } from "next/font/google";
 
 const dmSans = DM_Sans({
-  weight: ["400", "500", "700"],
-  style: ["italic", "normal"],
   subsets: ["latin"],
   variable: "--font-dmSans",
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
 });
 
 export default function App({ Component, pageProps }) {
-  return;
-  <main className={`${dmSans.variable} font-sans`}>
-    <Component {...pageProps} />
-  </main>;
+  return (
+    <main className={`${dmSans.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/sign-up/index.js
+++ src/pages/sign-up/index.js
@@ -1,7 +1,70 @@
-export default function index() {
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SignUp() {
   return (
-    <div>
-      <p>sign up page</p>
+    <div className="h-screen">
+      <div
+        id="form-container"
+        className="flex flex-col justify-center h-full px-[30px]"
+      >
+        <div className=" mb-[28px]">
+          <Image
+            priority
+            src="/images/logo.svg"
+            height={66}
+            width={66}
+            alt="logo"
+          />
+          <h2 className=" font-extrabold text-[40px] ">Sign Up</h2>
+          <p>
+            Already have an account?{" "}
+            <Link href="/sign-in">
+              <span className="underline text-[#745FF2]">Sign in</span>
+            </Link>
+          </p>
+        </div>
+        <form action="" className=" space-y-[22px] mb-[28px]">
+          <input
+            type="text"
+            placeholder="Enter Your Name *"
+            className="block placeholder:text-black border border-1 border-black px-[17px] py-[18px] rounded-lg w-[100%] placeholder:text-[20px] text-[20px]"
+          />
+          <input
+            type="text"
+            placeholder="Email *"
+            className="block placeholder:text-black border border-1 border-black px-[17px] py-[18px] rounded-lg w-[100%] placeholder:text-[20px] text-[20px]"
+          />
+          <input
+            type="text"
+            placeholder="Password *"
+            className="block placeholder:text-black border border-1 border-black px-[17px] py-[18px] rounded-lg w-[100%] placeholder:text-[20px] text-[20px]"
+          />
+          <input
+            type="text"
+            placeholder="Password (Confirm) *"
+            className="block placeholder:text-black border border-1 border-black px-[17px] py-[18px] rounded-lg w-[100%] placeholder:text-[20px] text-[20px]"
+          />
+        </form>
+        <div className=" flex gap-[20px] mb-[30px]">
+          <div className="">
+            <input
+              className="w-[14px] h-[14px]"
+              type="checkbox"
+              id="termsCheck"
+              name=""
+              value=""
+            />
+          </div>
+          <div>
+            <span>I agree to the Terms of Service and Privacy Policy</span>
+          </div>
+        </div>
+        <button className=" bg-[#7E635B]/50 py-[11px] rounded-full text-[#A89191]">
+          Create your free account
+        </button>
+      </div>
+      <div className="hidden">2</div>
     </div>
   );
 }
